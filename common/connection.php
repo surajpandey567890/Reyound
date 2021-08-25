580ecb313ece63894b0bea5c3a8f7fcefc72ee7b
surajpandey567890 <88534335+surajpandey567890@users.noreply.github.com>
2021-08-25 14:59:14 +0530

    changes

--- common/connection.php
+++ common/connection.php
@@ -5,7 +5,7 @@ class Connection{
 	public function __construct()
 	{
 		                     
-		$this->conn = new mysqli("localhost","root","","fashion_jalsa");
+		$this->conn = new mysqli("localhost","root","","reyound");
 		//$this->conn = new mysqli("185.28.21.1","u525569677_fashion_jalsa","Jalsa@12345","u525569677_fashion_jalsa");
 	}
 
